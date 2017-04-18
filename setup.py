e6c7a6136e1a7d54e0c643356dd3854346fabe0f
Sarath S Pillai <sarath.sp06@gmail.com>
2017-04-18 11:17:21 +0530

    Updated requirements file to have non strict depencency of requets >= 2.5.1 , addressing https://github.com/sarathsp06/exotel-py/issues/3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="exotel",
 
     # Version number (initial):
-    version="0.1.1",
+    version="0.1.4",
 
     # Application author details:
     author="sarath",
@@ -14,20 +14,50 @@ setup(
     # Packages
     packages=['exotel'],
 
-    # Include additional files into the package
-    include_package_data=True,
-
     # Details
     url="https://github.com/sarathsp06/exotel-py",
-    download_url = 'https://github.com/sarathsp06/exotel-py/archive/v0.1.1.tar.gz'
-    #
+    download_url = 'https://github.com/sarathsp06/exotel-py/archive/v0.1.3.tar.gz',
     license="LICENSE.txt",
     description="Python SDK for Exotel API",
-    long_description=open("README.rst").read(),
+    long_description="""
+   exotel-py
+---------
 
-    # Dependent packages (distributions)
-    install_requires=[
-        "requests==2.2.1",
-        "requests-oauthlib==0.5.0"
-    ],
+Python module for exotels call and sms api's
+
+installation
+~~~~~~~~~~~~
+
+``pip install exotel``
+
+Usage
+-----
+
+-  Initialize
+
+    ``from exotel import Exotel``
+
+    ``client = Exotel(sid,token)``
+
+- make call to connect a number to another
+
+    ``client.call_number('from_number','exophone','to_number')``
+
+-  make call to connect a number to a flow
+
+    ``client.call_flow('from_number','exophone','flow_id')``
+
+-  send an sms
+
+    ``client.sms('from_number',to_number',"sms_body")``
+
+- get details of an sms
+
+    ``client.sms_details('sms_sid')``
+
+- get details of a call
+
+    ``client.call_details('call_sid')``
+
+    """
 )
